3618be8d54ba209a78209b6229b35f897b6c0299
Sixto Ortega <sixto_miguel@hotmail.com>
2020-03-05 13:59:01 -0400

    Adding product repository

---
+++ NetCoreWeb-CRUDGraphQL/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreWeb_CRUDGraphQL.Data;
+using NetCoreWeb_CRUDGraphQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWeb_CRUDGraphQL.Repositories
+{
+    public class ProductRepository : IBaseRepository<Product>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(Product item)
+        {
+            _context.Products.Add(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Product item)
+        {
+            _context.Products.Remove(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(Guid id)
+        {
+            var store = await GetByIdAsync(id);
+            _context.Products.Remove(store);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(Guid id)
+        {
+            return await _context.Products.Where(x => x.ID == id).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(Product item)
+        {
+            _context.Products.Update(item);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
