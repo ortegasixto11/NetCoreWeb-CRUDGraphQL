b603792c452cd3f4c6a77229f6e5d48992330549
Sixto Ortega <sixto_miguel@hotmail.com>
2020-03-11 12:21:04 -0400

    Adding index page products

---
+++ CRUDGraphQL.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDGraphQL.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUDGraphQL.Frontend/wwwroot/js/api-service.js
@@ -0,0 +1,5 @@
+﻿const baseURL = "https://localhost:44369/api"
+
+async function asyncGet(url) {
+    return fetch(`${baseURL}${url}`).then(async (response) => await response.json())
+}
